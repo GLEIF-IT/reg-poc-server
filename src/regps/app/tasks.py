1ed0a895baa005248903cc9d1a685317ef912702
2byrds <2byrds@gmail.com>
2023-07-09 21:28:03 -0400

    Async result on put/post uses celery worker and redis
    Signed-off-by: 2byrds <2byrds@gmail.com>

--- src/regps/app/tasks.py
+++ src/regps/app/tasks.py
@@ -2,6 +2,7 @@
 
 import celery
 import falcon
+import json
 import os
 import requests
 from time import sleep
@@ -46,19 +47,19 @@ else:
         rurl = VERIFIER_REPORTS
 
 @app.task
-def check_login(aid) -> falcon.Response:
+def check_login(aid):
     print("checking login: aid {}".format(aid))
     gres = requests.get(aurl+f"{aid}", headers={"Content-Type": "application/json"})
     print("login status: {}".format(gres))
-    return gres
+    return serResp(gres)
 
 @app.task
-def verify(aid,said,vlei) -> falcon.Response:
+def verify(aid,said,vlei):
     # first check to see if we're already logged in
     print("Login verification started {} {}....".format(aid,said,vlei[:50]))
     gres = check_login(aid)
-    print("Login check {} {}....".format(gres.status_code,gres.text[:50]))
-    if str(gres.status_code) == str(falcon.http_status_to_code(falcon.HTTP_OK)):
+    print("Login check {} {}....".format(gres["status_code"],gres["text"][:50]))
+    if str(gres["status_code"]) == str(falcon.http_status_to_code(falcon.HTTP_OK)):
         print("already logged in")
         return gres
     else:
@@ -69,24 +70,24 @@ def verify(aid,said,vlei) -> falcon.Response:
             gres = None
             while(gres == None or gres.status_code == falcon.http_status_to_code(falcon.HTTP_404)):
                 gres = check_login(aid)
-                print("polling result {}".format(gres.text))
+                print("polling result {}".format(gres["text"]))
                 sleep (1)
-            return gres
+            return serResp(gres)
         else:
-            return pres
+            return serResp(pres)
         
 @app.task
-def check_upload(aid, dig) -> falcon.Response:
+def check_upload(aid, dig):
     print("checking upload: aid {} and dig {}".format(aid,dig))
     gres = requests.get(rurl+f"{aid}/{dig}", headers={"Content-Type": "application/json"})
     print("upload status: {}".format(gres))
-    return gres
+    return serResp(gres)
 
 @app.task
-def upload(aid,dig,contype,report) -> falcon.Response:
+def upload(aid,dig,contype,report):
     # first check to see if we've already uploaded
     gres = check_upload(aid,dig)
-    if gres.status_code == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
+    if gres["status_code"] == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
         print("already uploaded")
         return gres
     else:
@@ -95,10 +96,15 @@ def upload(aid,dig,contype,report) -> falcon.Response:
         print("post response {}".format(pres.text))
         if pres.status_code == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
             gres = None
-            while(gres == None or gres.status_code == falcon.http_status_to_code(falcon.HTTP_404)):
+            while(gres == None or gres["status_code"] == falcon.http_status_to_code(falcon.HTTP_404)):
                 gres = check_upload(aid,dig)
-                print("polling result {}".format(gres.text))
+                print("polling result {}".format(gres["text"]))
                 sleep (1)
             return gres
         else:
-            return pres
+            return serResp(pres)
+        
+def serResp(resp):
+    sResp = {"status_code": resp.status_code, "text": resp.text, "headers":{"Content-Type":resp.headers['Content-Type']}}
+    print("serializing response {}".format(sResp))
+    return sResp
