5dc3dd33646f55e13af31d2927f1f00c3f1b2125
Kevin Griffin <griffin.kev@gmail.com>
2023-07-20 11:46:30 -0400

    clean up

--- src/regps/app/tasks.py
+++ src/regps/app/tasks.py
@@ -2,7 +2,6 @@
 
 import celery
 import falcon
-import json
 import os
 import requests
 from time import sleep
@@ -21,90 +20,100 @@ if CELERY_BACKEND is None:
     
 app = celery.Celery('tasks', broker=CELERY_BROKER, backend=CELERY_BACKEND)
 
-aurl = "http://localhost:7676/authorizations/"
-purl = "http://localhost:7676/presentations/"
-rurl = "http://localhost:7676/reports/"
+auths_url = "http://127.0.0.1:7676/authorizations/"
+presentations_url = "http://127.0.0.1:7676/presentations/"
+reports_url = "http://127.0.0.1:7676/reports/"
 
 VERIFIER_AUTHORIZATIONS = os.environ.get('VERIFIER_AUTHORIZATIONS')
 if VERIFIER_AUTHORIZATIONS is None:
-        print(f"VERIFIER_AUTHORIZATIONS is not set. Using default {aurl}")
+        print(f"VERIFIER_AUTHORIZATIONS is not set. Using default {auths_url}")
 else:
         print(f"VERIFIER_AUTHORIZATIONS is set. Using {VERIFIER_AUTHORIZATIONS}")
-        aurl = VERIFIER_AUTHORIZATIONS
+        auths_url = VERIFIER_AUTHORIZATIONS
         
 VERIFIER_PRESENTATIONS = os.environ.get('VERIFIER_PRESENTATIONS')
 if VERIFIER_PRESENTATIONS is None:
-        print(f"VERIFIER_PRESENTATIONS is not set. Using default {purl}")
+        print(f"VERIFIER_PRESENTATIONS is not set. Using default {presentations_url}")
 else:
         print(f"VERIFIER_PRESENTATIONS is set. Using {VERIFIER_PRESENTATIONS}")
-        purl = VERIFIER_PRESENTATIONS
+        presentations_url = VERIFIER_PRESENTATIONS
 
 VERIFIER_REPORTS = os.environ.get('VERIFIER_REPORTS')
 if VERIFIER_REPORTS is None:
-        print(f"VERIFIER_REPORTS is not set. Using default {rurl}")
+        print(f"VERIFIER_REPORTS is not set. Using default {reports_url}")
 else:
         print(f"VERIFIER_REPORTS is set. Using {VERIFIER_REPORTS}")
-        rurl = VERIFIER_REPORTS
+        reports_url = VERIFIER_REPORTS
 
 @app.task
-def check_login(aid):
-    print("checking login: aid {}".format(aid))
-    gres = requests.get(aurl+f"{aid}", headers={"Content-Type": "application/json"})
-    print("login status: {}".format(gres))
-    return serResp(gres)
+def check_login(aid: str) -> dict:
+    return serialzie(_login(aid))
+
+def _login(aid: str) -> falcon.Response:
+    print("checking login: aid", aid)
+
+    gres = requests.get(f"{auths_url}{aid}", headers={"Content-Type": "application/json"})
+    print("login status:", gres)
+    return gres
 
 @app.task
-def verify(aid,said,vlei):
+def verify(aid: str, said: str, vlei: str) -> dict:
     # first check to see if we're already logged in
-    print("Login verification started {} {}....".format(aid,said,vlei[:50]))
-    gres = check_login(aid)
-    print("Login check {} {}....".format(gres["status_code"],gres["text"][:50]))
-    if str(gres["status_code"]) == str(falcon.http_status_to_code(falcon.HTTP_OK)):
+    print("Login verification started", aid, said, vlei[:50])
+
+    login_response = _login(aid)
+    print("Login check", login_response.status_code, login_response.text[:50])
+
+    if str(login_response.status_code) == str(falcon.http_status_to_code(falcon.HTTP_OK)):
         print("already logged in")
-        return gres
+        return serialzie(login_response)
     else:
-        print("putting to {}".format(purl+f"{said}"))
-        pres = requests.put(purl+f"{said}", headers={"Content-Type": "application/json+cesr"}, data=vlei)
-        print("put response {}".format(pres.text))
-        if pres.status_code == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
-            gres = None
-            while(gres == None or gres["status_code"] == falcon.http_status_to_code(falcon.HTTP_404)):
-                gres = check_login(aid)
-                print("polling result {}".format(gres["text"]))
+        print("putting to", presentations_url, said)
+        presentation_response = requests.put(f"{presentations_url}{said}", headers={"Content-Type": "application/json+cesr"}, data=vlei)
+        print("put response", presentation_response.text)
+
+        if presentation_response.status_code == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
+            login_response = None
+            while(login_response == None or login_response.status_code == falcon.http_status_to_code(falcon.HTTP_404)):
+                login_response = _login(aid)
+                print("polling result", login_response)
                 sleep (1)
-            return serResp(gres)
+            return serialzie(login_response)
         else:
-            return serResp(pres)
+            return serialzie(presentation_response)
         
 @app.task
-def check_upload(aid, dig):
-    print("checking upload: aid {} and dig {}".format(aid,dig))
-    gres = requests.get(rurl+f"{aid}/{dig}", headers={"Content-Type": "application/json"})
-    print("upload status: {}".format(gres))
-    return serResp(gres)
+def check_upload(aid: str, dig: str) -> dict:
+    return serialzie(_upload(aid, dig))
+
+def _upload(aid: str, dig: str) -> falcon.Response:
+    print("checking upload: aid {aid} and dig {dir}")
+    reports_response = requests.get(f"{reports_url}{aid}/{dig}", headers={"Content-Type": "application/json"})
+    print("upload status:", reports_response)
+    return reports_response
 
 @app.task
-def upload(aid,dig,contype,report):
+def upload(aid: str, dig: str, contype: str, report) -> dict:
+    print(type(report))
     # first check to see if we've already uploaded
-    gres = check_upload(aid,dig)
-    if gres["status_code"] == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
+    upload_response = _upload(aid, dig)
+    if upload_response.status_code == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
         print("already uploaded")
-        return gres
+        return serialzie(upload_response)
     else:
-        print("posting to {}".format(rurl+f"{dig}"))
-        pres = requests.post(rurl+f"{aid}/{dig}", headers={"Content-Type": contype}, data=report)
-        print("post response {}".format(pres.text))
-        if pres.status_code == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
-            gres = None
-            while(gres == None or gres["status_code"] == falcon.http_status_to_code(falcon.HTTP_404)):
-                gres = check_upload(aid,dig)
-                print("polling result {}".format(gres["text"]))
+        print("posting to", reports_url, dig)
+        presentation_response = requests.post(f"{reports_url}{aid}/{dig}", headers={"Content-Type": contype}, data=report)
+        print("post response", presentation_response.text)
+
+        if presentation_response.status_code == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
+            upload_response = None
+            while(upload_response == None or upload_response.status_code == falcon.http_status_to_code(falcon.HTTP_404)):
+                upload_response = _upload(aid,dig)
+                print("polling result", upload_response.text)
                 sleep (1)
-            return gres
+            return serialzie(upload_response)
         else:
-            return serResp(pres)
+            return serialzie(presentation_response)
         
-def serResp(resp):
-    sResp = {"status_code": resp.status_code, "text": resp.text, "headers":{"Content-Type":resp.headers['Content-Type']}}
-    print("serializing response {}".format(sResp))
-    return sResp
+def serialzie(response: falcon.Response) -> dict:
+    return {"status_code": response.status_code, "text": response.text, "headers":{"Content-Type": response.headers['Content-Type']}}
