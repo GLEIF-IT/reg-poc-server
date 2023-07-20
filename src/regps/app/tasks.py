e2ade03d1c307e740391f374b73067c1930a4482
2byrds <2byrds@gmail.com>
2023-07-20 16:31:52 -0400

    initial/hardcoded header verification in swagger
    Signed-off-by: 2byrds <2byrds@gmail.com>

--- src/regps/app/tasks.py
+++ src/regps/app/tasks.py
@@ -7,54 +7,62 @@ import os
 import requests
 from time import sleep
 
-dbrok = "redis://127.0.0.1:6379/0"
-dback = "redis://127.0.0.1:6379/0"
+# dbrok = "redis://127.0.0.1:6379/0"
+# dback = "redis://127.0.0.1:6379/0"
 
-CELERY_BROKER = os.environ.get('CELERY_BROKER')
-if CELERY_BROKER is None:
-    print(f"CELERY_BROKER is not set. Using default {dbrok}")
-    CELERY_BROKER = dbrok
-CELERY_BACKEND = os.environ.get('CELERY_BACKEND')
-if CELERY_BACKEND is None:
-    print(f"CELERY_BACKEND is not set. Using default {dback}")
-    CELERY_BACKEND = dback
+# CELERY_BROKER = os.environ.get('CELERY_BROKER')
+# if CELERY_BROKER is None:
+#     print(f"CELERY_BROKER is not set. Using default {dbrok}")
+#     CELERY_BROKER = dbrok
+# CELERY_BACKEND = os.environ.get('CELERY_BACKEND')
+# if CELERY_BACKEND is None:
+#     print(f"CELERY_BACKEND is not set. Using default {dback}")
+#     CELERY_BACKEND = dback
     
-app = celery.Celery('tasks', broker=CELERY_BROKER, backend=CELERY_BACKEND)
+# app = celery.Celery('tasks', broker=CELERY_BROKER, backend=CELERY_BACKEND)
 
 aurl = "http://localhost:7676/authorizations/"
 purl = "http://localhost:7676/presentations/"
-rurl = "http://localhost:7676/reports/"
+repurl = "http://localhost:7676/reports/"
+requrl = "http://localhost:7676/request/verify/"
 
-VERIFIER_AUTHORIZATIONS = os.environ.get('VERIFIER_AUTHORIZATIONS')
-if VERIFIER_AUTHORIZATIONS is None:
-        print(f"VERIFIER_AUTHORIZATIONS is not set. Using default {aurl}")
-else:
-        print(f"VERIFIER_AUTHORIZATIONS is set. Using {VERIFIER_AUTHORIZATIONS}")
-        aurl = VERIFIER_AUTHORIZATIONS
+# VERIFIER_AUTHORIZATIONS = os.environ.get('VERIFIER_AUTHORIZATIONS')
+# if VERIFIER_AUTHORIZATIONS is None:
+#         print(f"VERIFIER_AUTHORIZATIONS is not set. Using default {aurl}")
+# else:
+#         print(f"VERIFIER_AUTHORIZATIONS is set. Using {VERIFIER_AUTHORIZATIONS}")
+#         aurl = VERIFIER_AUTHORIZATIONS
         
-VERIFIER_PRESENTATIONS = os.environ.get('VERIFIER_PRESENTATIONS')
-if VERIFIER_PRESENTATIONS is None:
-        print(f"VERIFIER_PRESENTATIONS is not set. Using default {purl}")
-else:
-        print(f"VERIFIER_PRESENTATIONS is set. Using {VERIFIER_PRESENTATIONS}")
-        purl = VERIFIER_PRESENTATIONS
+# VERIFIER_PRESENTATIONS = os.environ.get('VERIFIER_PRESENTATIONS')
+# if VERIFIER_PRESENTATIONS is None:
+#         print(f"VERIFIER_PRESENTATIONS is not set. Using default {purl}")
+# else:
+#         print(f"VERIFIER_PRESENTATIONS is set. Using {VERIFIER_PRESENTATIONS}")
+#         purl = VERIFIER_PRESENTATIONS
 
-VERIFIER_REPORTS = os.environ.get('VERIFIER_REPORTS')
-if VERIFIER_REPORTS is None:
-        print(f"VERIFIER_REPORTS is not set. Using default {rurl}")
-else:
-        print(f"VERIFIER_REPORTS is set. Using {VERIFIER_REPORTS}")
-        rurl = VERIFIER_REPORTS
+# VERIFIER_REPORTS = os.environ.get('VERIFIER_REPORTS')
+# if VERIFIER_REPORTS is None:
+#         print(f"VERIFIER_REPORTS is not set. Using default {repurl}")
+# else:
+#         print(f"VERIFIER_REPORTS is set. Using {VERIFIER_REPORTS}")
+#         repurl = VERIFIER_REPORTS
+        
+# VERIFIER_REQUESTS = os.environ.get('VERIFIER_REQUESTS')
+# if VERIFIER_REQUESTS is None:
+#         print(f"VERIFIER_REQUESTS is not set. Using default {requrl}")
+# else:
+#         print(f"VERIFIER_REQUESTS is set. Using {VERIFIER_REQUESTS}")
+#         repurl = VERIFIER_REQUESTS
 
-@app.task
+# @app.task
 def check_login(aid):
     print("checking login: aid {}".format(aid))
     gres = requests.get(aurl+f"{aid}", headers={"Content-Type": "application/json"})
     print("login status: {}".format(gres))
     return serResp(gres)
 
-@app.task
-def verify(aid,said,vlei):
+# @app.task
+def verify_rep(aid,said,vlei):
     # first check to see if we're already logged in
     print("Login verification started {} {}....".format(aid,said,vlei[:50]))
     gres = check_login(aid)
@@ -76,14 +84,23 @@ def verify(aid,said,vlei):
         else:
             return serResp(pres)
         
-@app.task
+# @app.task
+def verify_req(req):
+    print("Request verification started {}....".format(req))
+    print("putting to {}".format(requrl+f"{req}"))
+    print(f"verify_req headers {req.headers}")
+    pres = requests.post(requrl+"EEXekkGu9IAzav6pZVJhkLnjtjM5v3AcyA-pdKUcaGei")
+    print("put response {}".format(pres.text))
+    return serResp(pres)
+        
+# @app.task
 def check_upload(aid, dig):
     print("checking upload: aid {} and dig {}".format(aid,dig))
-    gres = requests.get(rurl+f"{aid}/{dig}", headers={"Content-Type": "application/json"})
+    gres = requests.get(repurl+f"{aid}/{dig}", headers={"Content-Type": "application/json"})
     print("upload status: {}".format(gres))
     return serResp(gres)
 
-@app.task
+# @app.task
 def upload(aid,dig,contype,report):
     # first check to see if we've already uploaded
     gres = check_upload(aid,dig)
@@ -91,8 +108,8 @@ def upload(aid,dig,contype,report):
         print("already uploaded")
         return gres
     else:
-        print("posting to {}".format(rurl+f"{dig}"))
-        pres = requests.post(rurl+f"{aid}/{dig}", headers={"Content-Type": contype}, data=report)
+        print("posting to {}".format(repurl+f"{dig}"))
+        pres = requests.post(repurl+f"{aid}/{dig}", headers={"Content-Type": contype}, data=report)
         print("post response {}".format(pres.text))
         if pres.status_code == falcon.http_status_to_code(falcon.HTTP_ACCEPTED):
             gres = None
